6cd8b05ebe77ae29c5baf999ab29d10356aaf01e
Pascal <pdrzazga@numericable.fr>
2018-01-29 10:08:37 +0100

    new file:   main.m

---
+++ main.m
@@ -0,0 +1,24 @@
+%%
+clear, clc, close all;
+load('rawDatas.mat');
+
+%%
+color = suppressBackground(datas,1000);
+close;
+%%
+figure(1);
+
+%%
+handPositions = [];
+handImage = [];
+hold on;
+
+for i = 1:69
+    mask = skinColorBinarise(color(:, :, :, i));
+    [handImage, nbHands, barys] = extractHand(mask, i, datas.depth(i));
+    handPositions = kalmanHandTracking(barys, nbHands, barys);
+    rgb = insertMarker(handImage,[barys(1,2), barys(1,1)]);
+    figure1 = imshow(rgb);
+    pause(1/30);
+end
+
